be30aadef41ef92a3de85d31bbafe2f02ad195a7
Oscar Bernal <odbg2013@gmail.com>
2023-02-08 11:01:20 -0500

    add fetch react issues

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+
+const getLabelsAsync = async () => {
+	const res = await fetch('https://api.github.com/repos/facebook/react/labels')
+	const data = await res.json()
+	return data
+}
 
 export const LabelPicker = () => {
-  return (
-    <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
-    </div>
-  )
+	const labelPicker = () => {
+		const labelsQuery = useQuery(['labels'], getLabelsAsync)
+	}
+
+	return (
+		<div>
+			<span
+				className='badge rounded-pill m-1 label-picker'
+				style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+			>
+				Primary
+			</span>
+		</div>
+	)
 }
