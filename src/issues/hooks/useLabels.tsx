daab6062705eee03ec78d2e73fc5747f9c86cfab
Oscar Bernal <odbg2013@gmail.com>
2023-02-22 10:11:17 -0500

    add placeholder data

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -14,6 +14,17 @@ export const useLabels = () => {
 	const labelsQuery = useQuery(['labels'], getLabelsAsync, {
 		refetchOnWindowFocus: false,
 		staleTime: 1000 * 60 * 60,
+		placeholderData: [
+			{
+				id: 791921801,
+				node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
+				url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
+				name: '❤️',
+				color: 'ffffff',
+				default: false,
+				description: null,
+			},
+		],
 	})
 
 	return labelsQuery
